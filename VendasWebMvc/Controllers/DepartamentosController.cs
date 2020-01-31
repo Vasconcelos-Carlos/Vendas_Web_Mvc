ce8426b1d0fe85bb4f49735ac0d4b0329efadf83
Carlos Alberto <carlos98.camv@gmail.com>
2020-01-31 18:15:43 -0300

    Primeiro Model_View_Controller Departamento

---
+++ VendasWebMvc/Controllers/DepartamentosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VendasWebMvc.Models;
+namespace VendasWebMvc.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> Lista = new List<Departamento>();
+            Lista.Add(new Departamento { Id = 1, Nome = "Eletonicos" });
+            Lista.Add(new Departamento { Id = 2, Nome = "Moda" });
+
+            return View(Lista);
+        }
+    }
+}
--- VendasWebMvc/Controllers/HomeController.cs
+++ VendasWebMvc/Controllers/HomeController.cs
@@ -38,7 +38,7 @@ namespace VendasWebMvc.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new VendasWebMvc.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }

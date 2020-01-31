9035c483738e1e75869efbd96774a7e15e1b2222
Carlos Alberto <carlos98.camv@gmail.com>
2020-01-31 17:23:40 -0300

    Primeira pagina controller e razor teste

--- VendasWebMvc/Controllers/HomeController.cs
+++ VendasWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace VendasWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sistema de vendas web utilizadno MVC.";
+            ViewData["Desenvolvedor"] = "Carlos Alberto";
 
             return View();
         }

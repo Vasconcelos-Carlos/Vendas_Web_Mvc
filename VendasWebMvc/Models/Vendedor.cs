a5a7624c3eb7371e4cbb107c81806a4569289ebe
Carlos Alberto <carlos98.camv@gmail.com>
2020-02-06 17:00:20 -0300

    Localizacao da aplicaçao, Formatação de dados (Data, Salario Base, Email)

--- VendasWebMvc/Models/Vendedor.cs
+++ VendasWebMvc/Models/Vendedor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,22 @@ namespace VendasWebMvc.Models
     public class Vendedor
     {
         public int Id { get; set; }
+
         public string Nome { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime Aniversario { get; set; }
+
+        [Display(Name ="Salario Base")]
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public double SalarioBase { get; set; }
+
         public Departamento Departamento { get; set; }
         public int DepartamentoId { get; set; }
         public ICollection<RegistroVendas> Vendas { get; set; } = new List<RegistroVendas>();

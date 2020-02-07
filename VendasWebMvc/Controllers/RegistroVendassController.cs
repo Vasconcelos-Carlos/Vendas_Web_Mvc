6fd200b7c7113b99eed9981b3229d6f168aa3965
Carlos Alberto <carlos98.camv@gmail.com>
2020-02-06 23:42:44 -0300

    Preparando Views Busca de vendas

---
+++ VendasWebMvc/Controllers/RegistroVendassController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VendasWebMvc.Controllers
+{
+    public class RegistroVendassController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaSimples()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaAgrupada()
+        {
+            return View();
+        }
+
+    }
+}
